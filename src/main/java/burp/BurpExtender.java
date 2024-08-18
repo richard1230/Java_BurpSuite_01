5ffdb8cad87d6a2e1c898328ab64dffb2402a5d5
mac <mac@macdeMacBook-Pro.local>
2024-08-18 15:51:20 +0800

    first gui

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,41 +1,71 @@
 package burp;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.PrintWriter;
-import java.util.List;
 
 //BurpExtender为固定写法，入口类，，这些都是固定套路，包名也必须为burp,
 public class BurpExtender implements IBurpExtender, ITab {
     //https://portswigger.net/burp/extender/api/allclasses-noframe.html
     // 固定套路
     private IExtensionHelpers helpers;
-    private IBurpExtenderCallbacks callbacks;
+    private IBurpExtenderCallbacks cbs;
     private PrintWriter stdout;
+
+    public JPanel jPanelMain;
+
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 //        调用回调,固定套路
-        this.callbacks = callbacks;
+        this.cbs = callbacks;
 //        helpers需要初始化，获取helpers对象,固定套路
         this.helpers = callbacks.getHelpers();
 //        设置插件名称,固定套路
-        this.callbacks.setExtensionName("demo-02-burpsuite-plug ");
-//        在控制台打印
+        this.cbs.setExtensionName("demo-GUI-plug ");
+//        在插件控制台打印，固定套路
         this.stdout = new PrintWriter(callbacks.getStdout(), true);
-        stdout.println("hello world,this is gui plug!!!");
-//      这里一定要注册监听器,不然下面的函数无法运行
-//        callbacks.register(this);
+        stdout.println("hello,this is first gui plug!!!");
+
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                jPanelMain = new JPanel();
+
+                JButton jButton = new JButton("click me,please");
+
+                jButton.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+//                        super.mouseClicked(e);
+                        stdout.println("someone click me");
+                    }
+                });
+
+                // 将按钮添加到 主面板 jPanelMain 上.
+                jPanelMain.add(jButton);
+
+                // 设置自定义组件并添加标签
+                cbs.customizeUiComponent(jPanelMain);
+                cbs.addSuiteTab(BurpExtender.this);
+            }
+        });
+
+
     }
 
 
     @Override
     public String getTabCaption() {
-        return "";
+        return "Burp GUI 测试插件";
     }
 
     @Override
     public Component getUiComponent() {
-        return null;
+        return jPanelMain;
     }
 }
 
