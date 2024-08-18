9104493c277aa792df1c74b000e98dda44510cd7
mac <mac@macdeMacBook-Pro.local>
2024-08-18 16:50:23 +0800

    http process traffic redirector---》https://github.com/PortSwigger/example-traffic-redirector

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -7,14 +7,17 @@ import java.awt.event.MouseEvent;
 import java.io.PrintWriter;
 
 //BurpExtender为固定写法，入口类，，这些都是固定套路，包名也必须为burp,
-public class BurpExtender implements IBurpExtender, ITab {
+public class BurpExtender implements IBurpExtender ,IHttpListener{
     //https://portswigger.net/burp/extender/api/allclasses-noframe.html
     // 固定套路
     private IExtensionHelpers helpers;
     private IBurpExtenderCallbacks cbs;
     private PrintWriter stdout;
 
-    public JPanel jPanelMain;
+    private static final String HOST_FROM = "host1.example.org";
+    private static final String HOST_TO = "host2.example.org";
+
+
 
 
     @Override
@@ -24,48 +27,29 @@ public class BurpExtender implements IBurpExtender, ITab {
 //        helpers需要初始化，获取helpers对象,固定套路
         this.helpers = callbacks.getHelpers();
 //        设置插件名称,固定套路
-        this.cbs.setExtensionName("demo-GUI-plug ");
+        this.cbs.setExtensionName("traffic redirector ");
 //        在插件控制台打印，固定套路
         this.stdout = new PrintWriter(callbacks.getStdout(), true);
-        stdout.println("hello,this is first gui plug!!!");
+        stdout.println("traffic redirector  plug!!!");
+
+        //register ourselves as an HTTP listener
+        cbs.registerHttpListener(this);
+    }
 
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                jPanelMain = new JPanel();
+    @Override
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
 
-                JButton jButton = new JButton("click me,please");
+        if (messageIsRequest){
+            //获取当请求的http service
+            IHttpService httpService = messageInfo.getHttpService();
 
-                jButton.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-//                        super.mouseClicked(e);
-                        stdout.println("someone click me");
-                    }
-                });
-
-                // 将按钮添加到 主面板 jPanelMain 上.
-                jPanelMain.add(jButton);
-
-                // 设置自定义组件并添加标签
-                cbs.customizeUiComponent(jPanelMain);
-                cbs.addSuiteTab(BurpExtender.this);
+            // if the host is HOST_FROM, change it to HOST_TO
+            if (HOST_FROM.equalsIgnoreCase(httpService.getHost())){
+                messageInfo.setHttpService(helpers.buildHttpService(HOST_TO,httpService.getPort(),httpService.getProtocol()));
             }
-        });
+        }
 
-
-    }
-
-
-    @Override
-    public String getTabCaption() {
-        return "Burp GUI 测试插件";
-    }
-
-    @Override
-    public Component getUiComponent() {
-        return jPanelMain;
     }
 }
 
